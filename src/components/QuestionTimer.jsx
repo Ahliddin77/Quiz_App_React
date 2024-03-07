0ec0dec58643d93ee3fb13807944d39cdf38a91f
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-03-07 21:03:24 +0100

    using cleanup functions

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -2,13 +2,23 @@ import { useState, useEffect } from "react";
 
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
+
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
+
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id="queston-time" max={timeout} value={remainingTime} />;
